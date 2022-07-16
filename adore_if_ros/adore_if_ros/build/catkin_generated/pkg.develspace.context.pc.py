1706eec806577c680261207a59444255e3b115cd
Daniel Hess <Daniel.Hess@DLR.de>
2022-07-16 19:20:14 +0200

    rm build

--- adore_if_ros/adore_if_ros/build/catkin_generated/pkg.develspace.context.pc.py
+++
@@ -1,8 +0,0 @@
-# generated from catkin/cmake/template/pkg.context.pc.in
-CATKIN_PACKAGE_PREFIX = ""
-PROJECT_PKG_CONFIG_INCLUDE_DIRS = "/tmp/adore_if_ros/adore_if_ros/include".split(';') if "/tmp/adore_if_ros/adore_if_ros/include" != "" else []
-PROJECT_CATKIN_DEPENDS = "tf".replace(';', ' ')
-PKG_CONFIG_LIBRARIES_WITH_PREFIX = "".split(';') if "" != "" else []
-PROJECT_NAME = "adore_if_ros"
-PROJECT_SPACE_DIR = "/tmp/adore_if_ros/adore_if_ros/build/devel"
-PROJECT_VERSION = "0.2.0"
