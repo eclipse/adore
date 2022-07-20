e1b1bfc15f6e25438177081030e3c981bb21a0d1
Andrew Koerner <andrew.koerner@dlr.de>
2022-07-20 16:00:49 +0200

    DELETED adore_if_ros/build, updated adore_if_ros_msg and sumo_if_ros

--- adore_if_ros/adore_if_ros/build/CMakeFiles/adore_if_ros_msg_generate_messages_cpp.dir/cmake_clean.cmake
+++
@@ -1,5 +0,0 @@
-
-# Per-language clean rules from dependency scanning.
-foreach(lang )
-  include(CMakeFiles/adore_if_ros_msg_generate_messages_cpp.dir/cmake_clean_${lang}.cmake OPTIONAL)
-endforeach()
--- adore_if_ros/adore_if_ros/build/gtest/googletest/CTestTestfile.cmake
+++
@@ -1,6 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: /usr/src/googletest/googletest
-# Build directory: /tmp/adore_if_ros/adore_if_ros/build/gtest/googletest
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
