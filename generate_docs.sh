9e2832235d7833ac889086c4d6232a3791f01a78
Andrew Koerner <andrew.koerner@dlr.de>
2022-07-28 12:42:21 +0200

    added docnado generator

---
+++ generate_docs.sh
@@ -0,0 +1,21 @@
+#!/usr/bin/env bash
+
+function echoerr { echo "$@" >&2; exit 1;}
+SCRIPT_DIRECTORY="$( cd "$( dirname "${BASH_SOURCE[0]}" )" &> /dev/null && pwd )"
+
+pip install docnado --upgrade
+
+rm -rf documentation || true
+rm -rf docs || true
+mkdir docs
+
+main_branch=$(git symbolic-ref refs/remotes/origin/HEAD | sed 's@^refs/remotes/origin/@@')
+git checkout $main_branch -- documentation
+
+cp template/*.md docs
+cp documentation/*.md docs
+rm -rf documentation
+
+
+docnado --html docs
+git add docs
---
+++ template/home.md
@@ -0,0 +1,3 @@
+# ADORe
+
+Hello, ADORe!
