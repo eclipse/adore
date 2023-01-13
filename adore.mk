f56f85d848e5f053f9b877d9c00ad449b9fa561d
akoerner1 <andrew.koerner@dlr.de>
2023-01-13 14:17:14 +0100

    modified adore.mk for downstream consumpiton

--- adore.mk
+++ adore.mk
@@ -5,6 +5,8 @@ MAKEFLAGS += --no-print-directory
 
 .EXPORT_ALL_VARIABLES:
 
+ADORE_PROJECT=adore
+
 ADORE_MAKEFILE_PATH:=$(shell realpath "$(shell dirname "$(lastword $(MAKEFILE_LIST))")")
 MAKE_GADGETS_PATH:=${ADORE_MAKEFILE_PATH}/adore_if_ros_msg/make_gadgets
 REPO_DIRECTORY:=${ADORE_MAKEFILE_PATH}
