81560d82136cf7da133b83e5d49f953322748d4d
Andrew Koerner <99262104+akoerner1@users.noreply.github.com>
2023-03-21 15:09:47 +0100

    Feature/docker branch tagging (#52)
    * WIP: git branch as docker tag
    
    * WIP: docker branch tagging
    
    * WIP: docker branch tagging
    
    * WIP: docker branch tagging
    
    * Docker branch tagging V1
    
    * updated submodules
    
    * updated submodules
    
    * split adore-cli docker compose from main docker compose file.
    
    * updated adore cli help script and motd
    
    * updated adore.mk and adore-cli.mk
    
    * modified adore-cli dockerfile, added sudoers permission for apt, apt-get and aptitude for normal user
    
    * added absolute path refs to plotlabserver cd calls
    
    * updated plotlabserver, added make variable for docker-compose file
    
    * added env var to specify adore cli working dir
    
    * added vehicle name print statement to adore cli
    
    * modified adore.mk for downstream consumpiton
    
    * updated adore_if_ros_msg to fix the docker image caching load bug
    
    * track master for adore_if_ros_msg
    
    * track master for adore_if_ros_msg
    
    * added python-is-python3 system dep to adore-cli
    
    * added target/recipe to clean all submodules for adore_if_ros
    
    * fixed clean_submodules target/recipe for adore_if_ros
    
    * removed top level import of adore_if_ros_msg.mk
    
    * added realpath for catkin workspace gen
    
    * added apt to adore cli sudo permit list
    
    * added adore_if_v2x to the build party
    
    * wip
    
    * cleaned up print formatting on adore-cli help
    
    * updated clean targets/recipe
    
    * updated clean target/recipe
    
    * updated adore_if_v2x
    
    * updated apt_cacher_ng, updated clean target
    
    * added libadore as adore.mk dep
    
    * WIP
    
    * update apt_cacher_ng_docker
    
    * updated jenkinsfile
    
    * updated check_apt_caher_ng_docker
    
    * updated several submodules
    
    * WIP
    
    * updated submodules
    
    * updated submodules
    
    * updated libadore .dockerignore
    
    * updated submodules
    
    * added docker branch tagging feature make builds
    
    * added publish and caching to libadore external docker images
    
    * updated libadore external makefile
    
    * added docker publish, save, load for external libs
    
    * updated submods
    
    * fixed sementic error in plotlabserver log path
    
    * added clean for libadore_build/test and adore cli
    
    ---------
    
    Co-authored-by: alhadrad@gmail.com <akoerner>
    Co-authored-by: lobi_th <thomas.lobig@dlr.de>

---
+++ tools/adore-cli_help.sh
@@ -0,0 +1,41 @@
+#!/usr/bin/env bash
+
+set -euo pipefail
+
+echoerr (){ printf "%s" "$@" >&2;}
+exiterr (){ echoerr "$@"; exit 1;}
+
+SCRIPT_DIRECTORY="$( cd "$( dirname "${BASH_SOURCE[0]}" )" &> /dev/null && pwd )"
+
+printf "ADORe CLI Help\n"
+printf "  At any time you can run 'make help' to get a list of available make targets.\n"
+printf "\n"
+printf "  Environment: \n"
+printf "    ADORE_SOURCE_DIRECTORY: ${ADORE_SOURCE_DIRECTORY}\n"
+printf "    CATKIN_WORKSPACE_DIRECTORY: $(realpath "${CATKIN_WORKSPACE_DIRECTORY}")\n"
+printf "    ROS_ROOT: ${ROS_ROOT}\n"
+printf "    ROS_DISTRO: ${ROS_DISTRO}\n"
+printf "    ROS_MASTER_URI: ${ROS_MASTER_URI}\n"
+if [ -z ${VEHICLE_NAME+x} ]; then 
+    printf "    VEHICLE: no vehicle set\n"
+else 
+    printf "    VEHICLE: ${VEHICLE_NAME}\n" 
+fi
+
+printf "\n"
+
+printf "  Getting Started:\n\n"
+printf "    To execute a scenario run the following commands: \n"
+printf "      cd adore_if_ros_demos \n"
+printf "      roslaunch <scenario name> example: roslaunch baseline_test.launch\n\n"
+
+printf "    To attach another termanal session to the ADORe cli at any time on the host you can run: \n"
+printf "      'make adore-cli' or 'make cli' \n\n"
+
+printf "    To exit the adore cli type 'exit' \n\n"
+
+printf "    To stop the adore cli docker context type 'make stop_adore-cli' \n\n"
+
+printf "    All make commands must be run from the adore source directory: %s\n\n" "${ADORE_SOURCE_DIRECTORY}"
+
+printf "%b" "\e[1;31mWARNING: All file system changes made OUTSIDE of the adore source directory will NOT persist!\e[0m\n\n"
